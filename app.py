b7a3fd05b360f96bb8cc1e3af223a8b2a0fc48b2
Madiyar Moldabayev <madi020119@gmail.com>
2024-03-08 22:56:02 +0500

    Forgot about pericardialdiffusion

--- app.py
+++ app.py
@@ -9,25 +9,29 @@ model = xgboost.XGBClassifier(min_child_weight=3, max_depth=8, learning_rate=0.0
 model.load_model("xgboost_model.bin")  # Replace "xgboost_model.bin" with the actual file name of your model
 
 # Define the features required for prediction
-features = ['survival', 'age', 'fractionalshortening', 'epss', 'lvdd', 'wallmotion-score', 'wallmotion-index', 'mult']
+features = ['survival', 'age', 'pericardialeffusion', 'fractionalshortening', 'epss', 'lvdd', 'wallmotion-score', 'wallmotion-index', 'mult']
 
 # Create the Streamlit web app
 def main():
     st.title("Heart Disease Prediction")
 
     # Collect user input
-    survival = st.number_input("Enter Survival", min_value=9, max_value=31, value=20)
-    age = st.slider("Enter Age", min_value=45, max_value=81, value=50)
-    fractional_shortening = st.slider("Enter Fractional Shortening", min_value=0.0, max_value=0.5, value=0.15, step=0.01)
-    epss = st.slider("Enter EPSS Value", min_value=0.0, max_value=27.0, value=12.0, step=0.1)
-    lvdd = st.slider("Enter LVDD Value", min_value=3.0, max_value=7.0, value=5.0, step=0.1)
-    wallmotion_score = st.slider("Enter Wallmotion Score", min_value=5, max_value=24, value=14)
-    wallmotion_index = st.slider("Enter Wallmotion Index Value", min_value=1.0, max_value=1.5, value=1.0, step=0.1)
-    mult = st.slider("Enter Mult Value", min_value=0.5, max_value=1.0, value=0.75, step=0.01)
+    survival = st.number_input("Enter Survival", value=0, min_value=9, max_value=31, step=1)
+    age = st.number_input("Enter Age", value=50, min_value=45, max_value=81, step=1)
+    pericardial_effusion = st.selectbox("Pericardial Effusion", ["None", "Present"])
+    fractional_shortening = st.number_input("Enter Fractional Shortening", value=0.15, min_value=0.0, max_value=0.5, step=0.01)
+    epss = st.number_input("Enter EPSS Value", value=12.0, min_value=0.0, max_value=27.0, step=0.1)
+    lvdd = st.number_input("Enter LVDD Value", value=5.0, min_value=3.0, max_value=7.0, step=0.1)
+    wallmotion_score = st.slider("Enter Wallmotion Score", min_value=5, max_value=24, value=14, step=1)
+    wallmotion_index = st.slider("Enter Wallmotion Index Value", min_value=1.0, max_value=1.5, value=1.0, step=0.01)
+    mult = st.slider("Enter Mult Value", min_value=0.5, max_value=1.0, value=0.5, step=0.01)
 
     if st.button("Submit"):
+        # Convert pericardial effusion to binary (0 or 1)
+        pericardial_effusion = 1 if pericardial_effusion == "Present" else 0
+
         # Create a DataFrame from user input
-        user_data = pd.DataFrame([[survival, age, fractional_shortening, epss, lvdd, wallmotion_score, wallmotion_index, mult]], columns=features)
+        user_data = pd.DataFrame([[survival, age, pericardial_effusion, fractional_shortening, epss, lvdd, wallmotion_score, wallmotion_index, mult]], columns=features)
 
         # Standardize the user input data
         scaler = StandardScaler()
